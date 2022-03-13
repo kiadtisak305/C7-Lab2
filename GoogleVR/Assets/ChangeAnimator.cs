967497343871417b08756529d71bb82cf34de8a2
kiadtisak305 <kiadtisak.saesong123@gmail.com>
2022-03-13 15:07:22 +0700

    first commit

---
+++ GoogleVR/Assets/ChangeAnimator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAnimator : MonoBehaviour
+{
+    public void getAnimatorFlair() {
+        this.GetComponent<Animator>().Play("Flair");
+    }
+
+    public void getAnimatorHiphop()
+    {
+        this.GetComponent<Animator>().Play("Hip Hop Dancing");
+    }
+}
